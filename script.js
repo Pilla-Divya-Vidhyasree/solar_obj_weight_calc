ecc29f2921e3a08c4c8655d20235624be9f9743f
Pilla-Divya-Vidhyasree <113038544+Pilla-Divya-Vidhyasree@users.noreply.github.com>
2023-11-19 07:47:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,83 @@
-const mass = document.querySelector('.mass')
-var mass_value = mass.value;
-if(typeof mass_value == 'string'){
-mass.addEventListener('blur', e =>{
-    const numberMap = {
-        zero: 0,
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9,
-        ten: 10 // Add more as needed
+function weight_show() {
+    const planet = document.querySelector("#options");
+    const weight = document.querySelector(".mass");
+    const data = document.querySelector(".data");
+
+    // Check if the invalidData element already exists
+    let invalidData = document.querySelector("#invalidData");
+
+    if (planet.value == '--select planet--' || weight.value == '') {
+        // If invalidData doesn't exist, create a new element
+        if (!invalidData) {
+            invalidData = document.createElement("div");
+            invalidData.id = "invalidData";
+            data.appendChild(invalidData);
+        }
+        // Update the content of invalidData
+        invalidData.innerHTML = "* Enter both mass and planet details";
+        invalidData.style.color = 'red';
+    } else {
+        // If both mass and planet are selected, remove any existing error message
+        if (invalidData) {
+            invalidData.innerHTML = '';
+        }
+        const planet_img = document.querySelector('.image'); // Fix the selector here
+        planet_img.style.marginLeft = '10%';
+        const img_div = document.querySelector(".image_div")
+        img_div.style.display ='flex';
+        switch (planet.value) {
+            case 'Mercury':
+                planet_img.src = "https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/mercury.png";
+                break;
+        case 'Venus':
+            planet_img.src="https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/venus.png";
+            break;
+        case 'Earth':
+            planet_img.src="https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/earth.png";
+            break;
+        case 'Mars':
+            planet_img.src="https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/mars.png";
+            break;
+        case 'Jupiter':
+            planet_img.src="https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/jupiter.png";
+            break;
+        case 'Saturn':
+            planet_img.src="https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/saturn.png";
+            break;
+        case 'Uranus':
+            planet_img.src="https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/uranus.png";
+            break;
+        case 'Neptune':
+            planet_img.src="https://raw.githubusercontent.com/Asabeneh/30-Days-Of-JavaScript/master/24_Day_Project_solar_system/24_day_starter/images/neptune.png";
+            break;
+    }
+    const gravitationalRatios = {
+        "Mercury": 0.38,
+        "Venus": 0.91,
+        "Earth": 1.0,
+        "Mars": 0.38,
+        "Jupiter": 2.34,
+        "Saturn": 0.93,
+        "Uranus": 0.92,
+        "Neptune": 1.12,
     };
-    mass.value = numberMap[mass.value];
-})}
+
+
+        // Convert weight value to a numeric format
+        const numericWeight = parseFloat(weight.value);
+
+        // Check if the conversion is successful
+        if (!isNaN(numericWeight)) {
+            const weightOnPlanet = numericWeight * gravitationalRatios[planet.value];
+            const resultElement = document.querySelector(".displayData"); // Fix the selector here
+            resultElement.style.margin = '10%';
+            resultElement.style.color = 'white';
+            resultElement.style.fontSize = '30px';
+            resultElement.textContent = `Weight on ${planet.value}: ${weightOnPlanet.toFixed(2)} N`;
+
+        } else {
+            // Handle the case where weight is not a valid number
+            console.error("Invalid weight input");
+        }
+}
+}
